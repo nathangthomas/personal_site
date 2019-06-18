282dc3ce30b6b76df437a00a09fae012987a77a9
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-18 13:08:13 -0600

    Add test for error message when user tries to go to page that does not exist

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,13 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_gets_an_error_message
+    visit 'blahblahblah.com'
+
+    assert_equal 404, page.status_code
+    assert page.has_content?("Page not found.")
+
+  # I don't know where to put this. I tried putting it in it's own file however that also did not work properly.
+  end
 end
