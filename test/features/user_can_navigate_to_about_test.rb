5bbcf2b7517a5d51d3cab4522ecacc6f683115cc
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-18 13:07:23 -0600

    Remove test for non existing page

--- test/features/user_can_navigate_to_about_test.rb
+++ test/features/user_can_navigate_to_about_test.rb
@@ -1,15 +1,7 @@
 require './test/test_helper'
 
 class LinkTest < CapybaraTestCase
-  # def test_user_gets_an_error_message
-  #   visit '/'
-  #   click_on "About"
-  #
-  #   assert_equal 404, page.status_code
-  #   assert_equal '/about', current_path
-  #   assert page.has_content?("Page not found.")
-  # end
-  # I don't know where to put this. I tried putting it in it's own file however that also did not work properly. 
+ 
   def test_user_can_see_the_homepage
     visit '/'
     click_on "About"
